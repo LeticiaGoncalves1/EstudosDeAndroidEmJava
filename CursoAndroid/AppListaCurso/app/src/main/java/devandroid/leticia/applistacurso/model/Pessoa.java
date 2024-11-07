642e9532a47e8ef20c1ce0819343895113211414
Leticia Goncalves <contato.leticiabarcelos@gmail.com>
2024-11-06 23:11:17 -0300

    Feat.: Usando métodos getter e setters

--- CursoAndroid/AppListaCurso/app/src/main/java/devandroid/leticia/applistacurso/model/Pessoa.java
+++ CursoAndroid/AppListaCurso/app/src/main/java/devandroid/leticia/applistacurso/model/Pessoa.java
@@ -2,12 +2,57 @@ package devandroid.leticia.applistacurso.model;
 
 public class Pessoa {
 
+    //Atributos - Objetos da Classe Pessoa
+
+    private String nome;
+    private String sobrenome;
+    private String cursoDesejado;
+    private String telefone;
+    private String email;
+
     //Construtor
 
-    public Pessoa(){
+    public Pessoa(){}
 
+    //Métodos de acesso - Getters e Setters
 
-
+    public String getNome() {
+        return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- CursoAndroid/AppListaCurso/app/src/main/java/devandroid/leticia/applistacurso/view/MainActivity.java
+++ CursoAndroid/AppListaCurso/app/src/main/java/devandroid/leticia/applistacurso/view/MainActivity.java
@@ -13,7 +13,13 @@ import devandroid.leticia.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Objeto pessoa do tipo Pessoa
     Pessoa pessoa;
+    Pessoa pessoa2;
+
+    //Atributos do objeto pessoa do tipo String
+    String dadosPessoa;
+    String dadosPessoa2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +29,55 @@ public class MainActivity extends AppCompatActivity {
 
         pessoa = new Pessoa();
 
+
+        //Atribuindo dados ao objeto pessoa do tipo Pessoa
+        pessoa.setNome("Letícia");
+        pessoa.setSobrenome("Gonçalves");
+        pessoa.setCursoDesejado("Java Android");
+        pessoa.setTelefone("11981424242");
+        pessoa.setEmail("leticia@email.com");
+
+        pessoa2 = new Pessoa();
+        pessoa2.setNome("Vanessa");
+        pessoa2.setSobrenome("Moura");
+        pessoa2.setCursoDesejado("HTML e CSS");
+        pessoa2.setTelefone("47987654321");
+        pessoa2.setEmail("vanessa@email.com");
+
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobrenome();
+        dadosPessoa += " Curso desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone: ";
+        dadosPessoa += pessoa.getTelefone();
+        dadosPessoa += " Email: ";
+        dadosPessoa += pessoa.getEmail();
+
+        dadosPessoa2 = "Primeiro nome: ";
+        dadosPessoa2 += pessoa2.getNome();
+        dadosPessoa2 += " Sobrenome: ";
+        dadosPessoa2 += pessoa2.getSobrenome();
+        dadosPessoa2 += " Curso desejado: ";
+        dadosPessoa2 += pessoa2.getCursoDesejado();
+        dadosPessoa2 += " Telefone: ";
+        dadosPessoa2 += pessoa2.getTelefone();
+        dadosPessoa2 += " Email: ";
+        dadosPessoa2 += pessoa2.getEmail();
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
 
     }
 
